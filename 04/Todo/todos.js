4a643ade947830e84e62547e7f2f642870af10fa
Seong ho Hong <grohong76@gmail.com>
2017-10-07 19:11:36 +0900

    dom 설정

--- 04/Todo/todos.js
+++ 04/Todo/todos.js
@@ -1,1 +1,6 @@
-//start...
+var inputText = document.getElementById('inputText')
+var inputButton = document.getElementById('inputButton')
+
+inputButton.addEventListener('click', function(){
+  console.log(inputText.value)
+})
