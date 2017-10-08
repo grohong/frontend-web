fc0e0a591d256f46e2e9457ec8441760da510a2d
Seong ho Hong <grohong76@gmail.com>
2017-10-08 13:49:58 +0900

    html 이벤트 추가

--- 04/Todo/todos.js
+++ 04/Todo/todos.js
@@ -1,6 +1,17 @@
-var inputText = document.getElementById('inputText')
-var inputButton = document.getElementById('inputButton')
+function testfunc() {
 
-inputButton.addEventListener('click', function(){
-  console.log(inputText.value)
-})
+  var inputText = document.getElementById('inputText').value;
+  console.log(inputText);
+
+  // 콘솔 로그
+  return false;
+}
+
+function deletefunc(buttonElement) {
+  console.log(buttonElement);
+  var liElement = buttonElement.parentElement;
+  liElement.parentElement.removeChild(liElement);
+
+  //콘솔 로그
+  return false;
+}
