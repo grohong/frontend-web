15ed6a0ad0e295f68d389cde8b7c822ef2dbf125
Seong ho Hong <grohong76@gmail.com>
2017-10-08 14:11:33 +0900

    todoli generate 함수 구현

--- 04/Todo/todos.js
+++ 04/Todo/todos.js
@@ -1,17 +1,44 @@
-function testfunc() {
-
+function inputFunc() {
+  var todoList = document.getElementById('todoUl');
   var inputText = document.getElementById('inputText').value;
   console.log(inputText);
+
+  var todoList = generateTodoList(inputText);
 
   // 콘솔 로그
   return false;
 }
 
-function deletefunc(buttonElement) {
+function deleteFunc(buttonElement) {
   console.log(buttonElement);
   var liElement = buttonElement.parentElement;
   liElement.parentElement.removeChild(liElement);
 
   //콘솔 로그
   return false;
+}
+
+function generateTodoList(todo) {
+
+  var todoLi = document.createElement('li');
+
+  //li 속성값
+  var cancleButton = document.createElement('button');
+  cancleButton.className = "delete";
+  cancleButton.setAttribute(onclick, "deleteFunc(this);");
+  cancleButton.innerHTML = "x";
+
+  var checkInput = document.createElement('input');
+  checkInput.className = "toggle-checked";
+  checkInput.setAttribute(type, "checkbox");
+
+  var todoSpan = document.createElement('span');
+  todoSpan.className = "text";
+  todoSpan.innerHTML = todo;
+
+  todoLi.appendChild(cancleButton);
+  todoLi.appendChild(checkInput);
+  todoLi.appendChild(todoSpan);
+
+  return todoLi
 }
