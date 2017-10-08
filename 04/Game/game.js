f76b9ae3d2239c91a48e646a323461fba1c9ef2c
Seong ho Hong <grohong76@gmail.com>
2017-10-08 18:52:46 +0900

    box setTime 설정

--- 04/Game/game.js
+++ 04/Game/game.js
@@ -1,17 +1,22 @@
+var gameInfoDiv = document.getElementById('gameInfo');
 var gameFieldDiv = document.getElementById('gameField');
 var scoreSpan = document.getElementById('score');
 var levelSpan = document.getElementById('level');
 var missSpan = document.getElementById('miss');
+var boxDiv = document.getElementById('gameBox');
 
 function moveBox() {
-  var boxDiv = document.getElementById('gameBox');
-
   var boxLeft = Math.random() * gameFieldDiv.offsetWidth;
-  console.log(boxLeft);
-  console.log(boxDiv);
-  // boxDiv.setAttribute("left", parseInt(boxLeft));
-  boxDiv.style.left = parseInt(boxLeft) + "px";
+  //게임정보 div 높이를 더하여서 인포 안에 div 못들어 가게
+  var boxTop = (Math.random() * (gameFieldDiv.offsetHeight - gameInfoDiv.offsetHeight)) + gameInfoDiv.offsetHeight;
 
+  boxDiv.style.left = parseInt(boxLeft) + "px";
+  boxDiv.style.top = parseInt(boxTop) + "px";
+  boxDiv.style.backgroundColor = "red";
 }
 
-moveBox();
+boxDiv.addEventListener('click', function() {
+  boxDiv.style.backgroundColor = "blue";
+  console.log("click");
+  setTimeout(moveBox, 100);
+});
