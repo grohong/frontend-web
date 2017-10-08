89e16b215beee0d1d21b116c92bdcf3d9502cb60
Seong ho Hong <grohong76@gmail.com>
2017-10-08 18:07:56 +0900

    dom 설정

---
+++ 04/Game/game.js
@@ -0,0 +1,9 @@
+var gameFieldDiv = document.getElementById('gameField');
+var scoreSpan = document.getElementById('score');
+var levelSpan = document.getElementById('level');
+var missSpan = document.getElementById('miss');
+
+console.log(gameFieldDiv);
+console.log(scoreSpan);
+console.log(levelSpan);
+console.log(missSpan);
